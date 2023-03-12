cac6f16b622185b19c27358cd876edda7c2f8832
Lasma Poksane <laaasma.p@gmail.com>
2023-03-13 00:16:27 +0100

    build static SearchResults component

---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class SearchResults extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Results</h2>
+        {/* TrackList Component will be here */}
+      </div>
+    );
+  }
+}
+
+export default SearchResults;
