2a3dab5291454f2a47e7159f74752b89b193b7b5
Lasma Poksane <laaasma.p@gmail.com>
2023-03-14 08:17:21 +0100

    add initial App component

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span>mmm</span>ing
+        </h1>
+        <div>
+          {/* SearchBar goes here */}
+          <div>
+            {/* SearchResults go here */}
+            {/* Playlist goes here */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
