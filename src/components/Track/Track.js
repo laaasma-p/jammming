30a18a197b29994820d345f969aaa3b00b4568f8
Lasma Poksane <laaasma.p@gmail.com>
2023-03-14 08:13:33 +0100

    build initial Track component

---
+++ src/components/Track/Track.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class Track extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h3>Track name will go here</h3>
+          <p>Track artist will go here | track album will go here</p>
+        </div>
+        <button>+ or - will go here</button>
+      </div>
+    );
+  }
+}
+
+export default Track;
