3430bada926857859b60dc52487e8e8c848da02f
Lasma Poksane <laaasma.p@gmail.com>
2023-03-14 08:14:44 +0100

    style Track component

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,14 +1,15 @@
 import { Component } from "react";
+import "./Track.css";
 
 class Track extends Component {
   render() {
     return (
-      <div>
-        <div>
+      <div class="track">
+        <div class="track-information">
           <h3>Track name will go here</h3>
           <p>Track artist will go here | track album will go here</p>
         </div>
-        <button>+ or - will go here</button>
+        <button class="track-action">+ or - will go here</button>
       </div>
     );
   }
