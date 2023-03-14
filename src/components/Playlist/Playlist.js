a735aef2ff76d430477c0aac1cf3c537a17a76e8
Lasma Poksane <laaasma.p@gmail.com>
2023-03-14 08:25:27 +0100

    render TrackList component

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,12 +1,13 @@
 import { Component } from "react";
 import "./Playlist.css";
+import TrackList from "../TrackList/TrackList";
 
 class Playlist extends Component {
   render() {
     return (
       <div className="playlist">
         <input defaultValue={"New Playlist"} />
-        {/* TrackList Component will be here */}
+        <TrackList />
         <button className="playlist-save">SAVE TO SPOTIFY</button>
       </div>
     );
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,12 +1,13 @@
 import { Component } from "react";
 import "./SearchResults.css";
+import TrackList from "../TrackList/TrackList";
 
 class SearchResults extends Component {
   render() {
     return (
       <div className="search-results">
         <h2>Results</h2>
-        {/* TrackList Component will be here */}
+        <TrackList />
       </div>
     );
   }
