acd7cc3641b7dd0733c07acc71c8522f54b2dd8e
Lasma Poksane <laaasma.p@gmail.com>
2023-03-16 00:55:34 +0100

    passing addTrack method from App component

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -2,11 +2,25 @@ import { Component } from "react";
 import "./Track.css";
 
 class Track extends Component {
+  constructor(props) {
+    super(props);
+
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(event) {
+    this.props.onAdd(this.props.track);
+  }
+
   renderAction() {
     if (this.props.isRemoval) {
       return <button className="track-action">-</button>;
     }
-    return <button className="track-action">+</button>;
+    return (
+      <button className="track-action" onClick={this.addTrack}>
+        +
+      </button>
+    );
   }
 
   render() {
