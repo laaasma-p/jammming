028ee5ed173b93f27f0e9fd003f50b51d50619f5
Lasma Poksane <laaasma.p@gmail.com>
2023-03-18 19:57:24 +0100

    pass search method from App to SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,11 +2,23 @@ import { Component } from "react";
 import "./SearchBar.css";
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.search = this.search.bind(this);
+  }
+
+  search() {
+    this.props.onSearch(this.state.term);
+  }
+
   render() {
     return (
       <div className="searchbar">
-        <input placeholder="Enter A Song, Album, or Artist" />
-        <button className="search-button">SEARCH</button>
+        <input placeholder="Enter A Song Title" />
+        <button className="search-button" onClick={this.search}>
+          SEARCH
+        </button>
       </div>
     );
   }
