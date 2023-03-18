5383e570c032158f7e4facd3659178a545515d33
Lasma Poksane <laaasma.p@gmail.com>
2023-03-18 20:03:16 +0100

    create handleTermChange method and pass it to input

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -5,17 +5,29 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      term: "",
+    };
+
     this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
   }
 
   search() {
     this.props.onSearch(this.state.term);
   }
 
+  handleTermChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
   render() {
     return (
       <div className="searchbar">
-        <input placeholder="Enter A Song Title" />
+        <input
+          placeholder="Enter A Song Title"
+          onChange={this.handleTermChange}
+        />
         <button className="search-button" onClick={this.search}>
           SEARCH
         </button>
