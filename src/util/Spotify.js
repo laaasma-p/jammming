f01938ee68e48eafef8d44bc14b6cce6db985f9b
Lasma Poksane <laaasma.p@gmail.com>
2023-03-19 13:14:51 +0100

    create Spotify module to get the data from Spotify

---
+++ src/util/Spotify.js
@@ -0,0 +1,28 @@
+let accessToken;
+const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID; // Insert your own clientId from Spotify when using the application
+const redirectUri = "http://localhost:3000";
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      console.log(clientId);
+      return accessToken;
+    }
+
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/");
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = accessUrl;
+    }
+  },
+};
+
+export default Spotify;
