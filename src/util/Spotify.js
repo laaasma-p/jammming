ef9f439a4a727a378630e0f9beda0f2f40dda3a0
Lasma Poksane <laaasma.p@gmail.com>
2023-03-19 13:31:09 +0100

    create search method to search Spotify songs

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -23,6 +23,33 @@ const Spotify = {
       window.location = accessUrl;
     }
   },
+
+  search(term) {
+    const accessToken = Spotify.getAccessToken();
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (!jsonResponse.tracks) {
+          return [];
+        }
+
+        return jsonResponse.tracks.items.map((track) => {
+          return {
+            id: track.id,
+            name: track.name,
+            artists: track.artists[0].name,
+            album: track.album.name,
+            uri: track.uri,
+          };
+        });
+      });
+  },
 };
 
 export default Spotify;
